13ceb6d4f4f0007bb07e8730e7dfb023c87510e9
MartaGajewska84 <57986204+MartaGajewska84@users.noreply.github.com>
2025-05-07 20:15:24 +0200

    Update init_database.sql

--- sql/cram-school-project/scripts/init_database.sql
+++ sql/cram-school-project/scripts/init_database.sql
@@ -1,1 +1,14 @@
+-- Create the cram_school database 
+
+CREATE DATABASE cram_school;
+   
+-- NOTE: After running this, switch to the cram_school database before executing the rest.
+
+-- =====================================================
+-- PostgreSQL DDL Script: Create Bronze, Silver and Gold Layer 
+-- =====================================================
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
 
